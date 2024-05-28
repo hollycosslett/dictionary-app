3eb0e4339ddeabbfe0476bf0274c71f5cd2fb953
hollycosslett <154456339+hollycosslett@users.noreply.github.com>
2024-05-28 10:12:35 +0100

    Created new component with form and search function

---
+++ src/Dictionary.js
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+import "./Dictionary.css";
+
+export default function Dictionary() {
+  const [keyword, setKeyword] = useState("");
+  function search(event) {
+    event.preventDefault();
+    alert(`Searching for ${keyword} `);
+  }
+  function handleKeywordChange(event) {
+    event.preventDefault();
+    setKeyword(event.target.value);
+  }
+
+  return (
+    <div className="Dictionary">
+      <form onSubmit={search}>
+        <input
+          type="search"
+          placeholder="Enter a word"
+          onChange={handleKeywordChange}
+        />
+        <input type="submit" value="Submit" />
+      </form>
+    </div>
+  );
+}
