26c550b41c5c880f0e328b7817e6c4457d11406c
hollycosslett <154456339+hollycosslett@users.noreply.github.com>
2024-06-07 07:24:00 +0100

    displayed the phonetics

--- src/Results.js
+++ src/Results.js
@@ -6,13 +6,16 @@ export default function Results(props) {
     return (
       <div className="Results">
         <h2>{props.results.word}</h2>
-        {props.results.meanings.map(function (meaning, index) {
-          return (
-            <div key={index}>
-              <Meaning meaning={meaning} />
-            </div>
-          );
-        })}
+        <p>{props.results.phonetic}</p>
+        <p>
+          {props.results.meanings.map(function (meaning, index) {
+            return (
+              <div key={index}>
+                <Meaning meaning={meaning} />
+              </div>
+            );
+          })}
+        </p>
       </div>
     );
   } else {
